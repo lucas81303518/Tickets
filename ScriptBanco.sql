b03f2d0719bafa51db63997a1ca0dc1556ddfef8
Lucas Ferreira <lucas_81303518@live.com>
2024-09-14 00:13:51 -0300

    ..

--- ScriptBanco.sql
+++ ScriptBanco.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE "TicketsDB";
+
 CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
     "MigrationId" character varying(150) NOT NULL,
     "ProductVersion" character varying(32) NOT NULL,
